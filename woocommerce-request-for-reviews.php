1adf3a64f3e55afa506a8d1470548e6c80f21800
abhishek-pokhriyal <pokhi@coloredcow.com>
2020-11-08 18:13:51 +0530

    Scaffold plugin

---
+++ woocommerce-request-for-reviews.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Woocommerce Request For Reviews
+ * Plugin URI:      https://github.com/abhishek-pokhriyal/woocommerce-request-for-reviews
+ * Description:     A WooCommerce extension that helps you request your customers to review products.
+ * Author:          ColoredCow
+ * Author URI:      https://coloredcow.com
+ * Text Domain:     woocommerce-request-for-reviews
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Woocommerce_Request_For_Reviews
+ */
+
+// Your code starts here.
